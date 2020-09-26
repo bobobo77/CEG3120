b1c236a8878a2dfdc6805d744e8dd7a82117dd6f
bobobo77 <bobo77b0.7@gmail.com>
2020-09-26 04:27:14 +0000

    Rebuilt from memory. lost it an hour ago

--- scripts/project1.sh
+++ scripts/project1.sh
@@ -1,9 +1,24 @@
-!# /bin/bash
-#permissions
-chmod a=r-x project1.sh
+# /bin/bash
 
-./project1.sh
+#lost data so starting from scratch, seeing as my work earlier was doo-doo
+#comments will be BELOW the commands
 
-export PATH=$PATH:/CEG3120/scripts/project1.sh
+if [  -d "/CEG3120/scripts"] && [ ":$PATH:" != *"/CEG3120/scripts"* ]
+#	if statement checking if the directory path already exists and/or exists in $PATH
+then 
+export PATH="$PATH: /CEG3120/scripts"
+#adding to the path
+echo 'export PATH="$PATH: /CEG3120/scripts"' >> $HOME/.profile
+echo 'bash proect1.sh' >> $HOME/.profile
+#appending to the .profile
+echo "reload your .profile folder to make changes"
 
+else
+words="My message is to not give up"
+echo $words >> ~/ect/updatemotd.d/00-header
 
+alias cheer="I know you can do it!"
+echo "type 'cheer' for some motivation"
+
+go get github.com/vim-volt/volt
+#command straight from the Volt plugin manager README.md
